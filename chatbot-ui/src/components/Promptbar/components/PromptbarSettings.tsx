944f2225894e16a597bca03f9f03474385f6cd6b
Fin-Alan <insanetechie10@gmail.com>
2023-07-10 15:19:39 +0530

    changed task select component

--- chatbot-ui/src/components/Promptbar/components/PromptbarSettings.tsx
+++ chatbot-ui/src/components/Promptbar/components/PromptbarSettings.tsx
@@ -1,11 +1,7 @@
-import { FC, useState, useContext } from "react";
+import { FC, useContext, ChangeEvent } from "react";
 import HomeContext from "@/pages/home/home.context";
 
 import PublicPrivateSwitch from "@/components/PublicPrivateSwitch";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { Tile, TilesList } from "@/types/tiles";
 
 interface Props {}
@@ -16,7 +12,7 @@ export const PromptbarSettings: FC<Props> = () => {
     handleSelectedTile,
   } = useContext(HomeContext);
 
-  const handleChange = (event: SelectChangeEvent) => {
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     handleSelectedTile(
       TilesList.find((tile) => tile.task == event.target.value) as Tile
     );
@@ -26,43 +22,27 @@ export const PromptbarSettings: FC<Props> = () => {
     <>
       {selectedConversation?.messages.length != 0 ? (
         <div className="border-t border-white/40 pt-6">
-          <FormControl fullWidth sx={{ color: "white" }}>
-            <InputLabel
-              id="demo-simple-select-label"
-              sx={{
-                color: "white",
-                "&.MuiOutlinedInput-root ": {
-                  color: "white",
-                  outline: "white",
-                },
-              }}
-            >
-              Task
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              sx={{
-                color: "white",
-                "&.MuiOutlinedInput-root ": {
-                  color: "white",
-                  outline: "white",
-                },
-                "&.MuiSelect-iconOutlined": {
-                  color: "white !important",
-                },
-              }}
-              value={selectedTile.task}
-              label="Task"
-              onChange={handleChange}
-            >
-              {TilesList.map((tile, index) => (
-                <MenuItem key={index} value={tile.task}>
-                  {tile.displayName}
-                </MenuItem>
+          <label
+            htmlFor="tilelist"
+            className="block mb-2 text-sm font-medium text-white"
+          >
+            Select Task
+          </label>
+          <select
+            id="tilelist"
+            value={selectedTile.task}
+            className="bg-[#343541]  text-white border border-gray-600 placeholder-gray-400 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5"
+            onChange={handleChange}
+          >
+            {TilesList.map((tile, index) => (
+              <>
+                <option value={tile.task} key={index} className="py-2">{tile.displayName}
+                </option>
+                
+              </>
+
               ))}
-            </Select>
-          </FormControl>
+          </select>
           <PublicPrivateSwitch size={25} />
         </div>
       ) : (
