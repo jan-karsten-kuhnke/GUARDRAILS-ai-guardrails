5efcf0653628b722d4bde597dea95e69ee921cdf
Harshita Badlani <harshita.badlani@synergy-ai.us>
2023-08-01 16:22:52 +0530

    took prompts from db in visualization chain

--- api/src/app/executors/chains/VisualizationChain.py
+++ api/src/app/executors/chains/VisualizationChain.py
@@ -11,67 +11,22 @@ import logging
 from executors.wrappers.SQLSequentialChainWrapper import SQLDatabaseSequentialChain
 from executors.wrappers.SqlWrapper import SqlWrapper
 from executors.utils.LlmProvider import LlmProvider
-
 from langchain.chains import LLMChain
 import sqlalchemy
 import json
 from sqlalchemy import create_engine,text
 from sqlalchemy.orm import sessionmaker
 from langchain.output_parsers.list import CommaSeparatedListOutputParser
+from executors.utils.ParamsProvider import ParamsProvider
 
 class VisualizationChain:
+    params=ParamsProvider.get_params('qa-viz')
+    PROMPT_SUFFIX =params['promptSuffix']
+
+    _DEFAULT_TEMPLATE = params['defaultTemplate']
     
-    PROMPT_SUFFIX = """Only use the following tables:
-    {table_info}
+    _DECIDER_TEMPLATE = params['deciderTemplate']
 
-    Question: {input}"""
-
-    _DEFAULT_TEMPLATE = """Given an input question, the SQL Agent should create a syntactically correct {dialect} query to run and return the answer based on the query results.
-
-    Here are the guidelines for building the query:
-
-    1. Always limit the query to at most {top_k} results, unless the user specifies a specific number of examples they wish to obtain.
-    2. Order the results by a relevant column to return the most interesting examples in the database.
-    3. Do NOT create any queries that can manipulate the database, such as INSERT, UPDATE, DELETE, ALTER. If the user asks for a query that manipulates the database, return an error message in the answer.
-    4. Use the correct join type and join condition when using joins. Make sure to refer to the correct column names.
-    5. Handle situations where two tables have the same column name to avoid ambiguity in the query.
-    6. Utilize subqueries to break down the problem into smaller steps when necessary.
-    7. For string comparisons, perform a lookup in the table to find the best match for the string, and then use the ID of the best match in the query.
-    8. Always terminate the SQL Query with a semi-colon.
-    9. In the resultset, include the name columns of the entity instead of the entity ID in the select clause.
-    10. If the SQLResult is more than 20 rows, return only the top 5 rows as a markdown table and ignore the rest of the rows. Add the following statement to the response: 'Result dataset is too large, showing only a part of the resultset, please use the query in a SQL agent to see the full result dataset.'
-    11. Never query for all the columns from a specific table; only ask for a few relevant columns given the question.
-    12. Pay attention to using only the column names visible in the schema description. Avoid querying for columns that do not exist. Also, be mindful of which column is in which table.
-
-    Use the following format for the query:
-
-
-        Question: Question here
-        SQLQuery: SQL Query to run
-        SQLResult: Result of the SQLQuery
-        Answer: Final answer here
-        """
-
-    _DECIDER_TEMPLATE = """Given the below input question and list of  tables that could be used to get the results, consider the following instructions which will be used to generate a query, based on the input, list of tables and the instructions,  output a comma separated list of the table names that may be necessary to answer this question.
-
-    Instructions:
-    1. Order the results by a relevant column to return the most interesting examples in the database.
-    2. Do NOT create any queries that can manipulate the database, such as INSERT, UPDATE, DELETE, ALTER. If the user asks for a query that manipulates the database, return an error message in the answer.
-    3. Use the correct join type and join condition when using joins. Make sure to refer to the correct column names.
-    4. Handle situations where two tables have the same column name to avoid ambiguity in the query.
-    5. Utilize subqueries to break down the problem into smaller steps when necessary.
-    6. For string comparisons, perform a lookup in the table to find the best match for the string, and then use the ID of the best match in the query.
-    7. Always terminate the SQL Query with a semi-colon.
-    8. In the resultset, include the name columns of the entity instead of the entity ID in the select clause.
-    9. If the result dataset is more than 20 rows, return only the top 5 rows as a markdown table and ignore the rest of the rows. Add the following statement to the response: 'Result dataset is too large, showing only a part of the resultset, please use the query in a SQL agent to see the full result dataset.'
-    10. Never query for all the columns from a specific table; only ask for a few relevant columns given the question.
-    11. Pay attention to using only the column names visible in the schema description. Avoid querying for columns that do not exist. Also, be mindful of which column is in which table.
-
-    Question: {query}
-
-    Table Names: {table_names}
-
-    Relevant Table Names:"""
     DECIDER_PROMPT = PromptTemplate(
         input_variables=["query", "table_names"],
         template=_DECIDER_TEMPLATE,
@@ -82,7 +37,7 @@ class VisualizationChain:
         input_variables=["input", "table_info", "dialect", "top_k"],
         template=_DEFAULT_TEMPLATE + PROMPT_SUFFIX,
     )
-    
+    print(params['vegaLitePrompt'],"params")
 
     def execute(self, query, is_private, chat_history):
         # conn_str = f"postgresql+psycopg2://postgres:1234@localhost:5432/AdventureWorks"
@@ -114,23 +69,7 @@ class VisualizationChain:
             )
             sql_query = sql_results[0]
             sql_data = sql_results[1].split("\nAnswer:")[0]
-            VEGA_LITE_PROMPT = """You are a great assistant at vega-lite visualization creation. No matter what the user ask, you should always response with a valid vega-lite specification in JSON.
-
-            You have been given a user question and  a sql query that answers that question, you also have been given a sample result of the sql query.
-
-            You should create the vega-lite specification based on user's query.
-
-            Besides, Here are some requirements:
-            1. Do not contain the key called 'data' in vega-lite specification.
-            2. If the user ask many times, you should generate the specification based on the previous context.
-            3. You should consider to aggregate the field if it is quantitative and the chart has a mark type of react, bar, line, area or arc.
-            4. Do not contain any text in the response apart from the vega-lite specification in JSON.
-            5. Do not use any aggregations on the datasets in the vega-lite specification, the aggregation should have been already done in the SQL query.
-
-            User Question: {question}
-            SQL Query: {sql_query}
-            SQL Result: {sql_data}
-"""
+            VEGA_LITE_PROMPT = params['vegaLitePrompt']
 
             vega_prompt = PromptTemplate(
                 input_variables=["question", "sql_query", "sql_data"],
