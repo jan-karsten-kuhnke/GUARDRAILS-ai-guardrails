93606295e257c09a20a59f30989d20abbac74f54
Tim-Kane-65641 <mohdtaha.khan@synergy-ai.us>
2023-08-09 17:16:30 +0530

    Move User Prompts and folders from mongo to PG

--- api/src/app/service/userdata_service.py
+++ api/src/app/service/userdata_service.py
@@ -1,35 +1,23 @@
 from oidc import get_current_user_email
-from repo.db import folders_context,prompts_context
 from database.models import ChainEntity
 from database.postgres import session
 from oidc import get_current_user_groups
 from integration.flowable_wrapper import flowable_wrapper
+from database.repository import Persistence
 
 class userdata_service:
     def get_all_folders(user_email):
-        return folders_context.get_folder_data(user_email)
+        return Persistence.get_folder_data(user_email)
         
-        
-    
     def upsert_folders(folders,user_email):
-        user_folder_data = {
-            "user_email": user_email,
-            "folders": folders
-        }
-        folders_context.upsert_folders_by_user_email(user_folder_data,user_email)
+        Persistence.upsert_folders_by_user_email(folders,user_email)
 
 
     def get_all_prompts(user_email):
-        return prompts_context.get_prompts_data(user_email)
-        
-        
+        return Persistence.get_prompts_data(user_email)
     
     def upsert_prompts(prompts,user_email):
-        user_prompts_data = {
-            "user_email": user_email,
-            "prompts": prompts
-        }
-        prompts_context.upsert_prompts_by_user_email(user_prompts_data,user_email)
+        Persistence.upsert_prompts_by_user_email(prompts,user_email)
 
 
 
