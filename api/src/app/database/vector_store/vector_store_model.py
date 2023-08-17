e4c27edc194ac99fa8d6fffc748499ef5026c1e6
CodeCrafter7 <CodeCrafter7@gmail.com>
2023-08-17 21:27:57 +0530

    changes to vector store table

--- api/src/app/database/vector_store/vector_store_model.py
+++ api/src/app/database/vector_store/vector_store_model.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Text,MetaData,Boolean, TIMESTAMP
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import JSONB,JSON
 from sqlalchemy.sql import func
 from globals import Globals
 
@@ -13,10 +13,12 @@ Vector_Base = declarative_base(metadata=metadata_obj)
 class CollectionEntity(Vector_Base):
     __tablename__ = 'langchain_pg_collection'
     uuid = Column(UUID(as_uuid=True), primary_key=True, server_default=func.gen_random_uuid())
+    cmetadata = Column(JSON, nullable=True, default=None)
     name =  Column(String(255))
     
     def to_dict(self):
         return {
             'uuid': str(self.uuid),
-            'name': self.name
+            'name': self.name,
+            'cmetadata': self.cmetadata
         }
