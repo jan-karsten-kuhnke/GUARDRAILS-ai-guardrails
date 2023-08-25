821bfb31cc64ade004334387996d6b7fd42c7ae1
Harshita Badlani <harshita.badlani@synergy-ai.us>
2023-08-25 14:08:36 +0530

    added new chain and prompts

--- api/src/app/executors/applet/Conversation.py
+++ api/src/app/executors/applet/Conversation.py
@@ -2,10 +2,11 @@ import json
 import random
 from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
+from langchain.chains import LLMChain
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.llms import  VertexAI
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 from globals import Globals
 from executors.utils.LlmProvider import LlmProvider
 from typing import Any
@@ -16,14 +17,25 @@ from executors.utils.AppletResponse import AppletResponse
 class Conversation:
     
     def execute(self, query, is_private, chat_history):
-        memory = ConversationBufferMemory(return_messages=True)
+        memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
         for history in chat_history:
             memory.save_context({"input": history[0]}, {"output": history[1]})
         
         llm=LlmProvider.get_llm(is_private=is_private, use_chat_model=True, max_output_token=1000, increase_model_token_limit=True)
         
-        chain = ConversationChain(
+        _DEFAULT_TEMPLATE = """You are ChatGPT, a large language model trained by OpenAI, based on the GPT-3.5 architecture , and you like explaining in detail , so answer in detail as possible.\nCurrent date: 2023-03-02.
+
+        {chat_history}
+        Human: {input}
+        Assistant:"""
+
+        PROMPT = PromptTemplate(
+        input_variables=["chat_history","input"], template=_DEFAULT_TEMPLATE
+        )
+            
+        chain = LLMChain(
             llm=llm,verbose=True,memory=memory,
+            prompt=PROMPT
         )
        
         # Prepare the chain
