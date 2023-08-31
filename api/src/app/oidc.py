bcc836c2f562dbc8b71f0abe9e4ba36c00053136
CodeCrafter7 <CodeCrafter7@gmail.com>
2023-08-31 06:20:46 +0530

    allow multiple definitions of user groups

--- api/src/app/oidc.py
+++ api/src/app/oidc.py
@@ -1,5 +1,6 @@
 from flask_oidc import OpenIDConnect
 from flask import request
+import logging
 oidc = OpenIDConnect()
 
 
@@ -10,9 +11,20 @@ def get_current_user_email():
     return user_info['email']
 
 def get_current_user_groups():
+    user_groups = []
     token = request.headers['authorization'].split(' ')[1]
     user_info = oidc._get_token_info(token)
-    return user_info['groups'] if 'groups' in user_info else []
+    groups = user_info['groups'] if 'groups' in user_info else []
+    for group in groups:
+        # if group  is of type string then append it to user_groups, if object, and contains code, append code to user_groups
+        if isinstance(group, str):
+            user_groups.append(group)
+        elif 'code' in group:
+            user_groups.append(group['code'])
+    logging.debug(f'User groups: {user_groups}')
+    return user_groups
+        
+
 
 def get_current_user_roles():
     token = request.headers['authorization'].split(' ')[1]
