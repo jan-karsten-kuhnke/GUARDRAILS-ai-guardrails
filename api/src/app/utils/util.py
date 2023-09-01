32aebf37e318e6943880742c34bbea4f351493d5
Shankar Kethavath <shankar.kethavath@synergy-ai.us>
2023-09-01 15:13:21 +0530

    Document controller validation

--- api/src/app/utils/util.py
+++ api/src/app/utils/util.py
@@ -1,4 +1,5 @@
 import datetime
+from flask import jsonify
 
 def rename_id(data):
         if isinstance(data, list):
@@ -17,3 +18,25 @@ def log(class_name = None, msg_type= None, content=None):
             'message': f'{msg_type} {content}'
         }
         return result
+
+    
+#validate collectin name
+def validate_collection_name(collection_name):
+    if not collection_name:
+        return jsonify(error="Collection name is required."),400
+    if type(collection_name) != str:
+        return jsonify(error="Collection name should be a string"),400
+    collection_name=collection_name.strip()
+    if collection_name.isalpha() == False:
+        return jsonify(error="Collection name should contain only alphabetic characters"),400
+    return True
+def validate_document_fields(title,description,location,folder_id):
+    if title and type(title) != str:
+        return jsonify(error="Invalid data type for title."),400 
+    if description and type(description) != str:
+        return jsonify(error="Invalid data type for description"),400
+    if location and type(location) != str:
+        return jsonify(error="Invalid data type for location"),400
+    if folder_id and type(folder_id) != int:
+        return jsonify(error="Invalid data type for folder_id"),400
+    return True
