9a7bff42398f780312d0288354aa88773c3503cc
Tim-Kane-65641 <mohdtaha.khan@synergy-ai.us>
2023-09-01 21:49:15 +0530

    initial setup

--- api/src/app/app.py
+++ api/src/app/app.py
@@ -2,15 +2,16 @@ import logging
 from dotenv import load_dotenv
 load_dotenv(override=False)
 from flask import Flask, render_template, make_response, jsonify, request
-from flask_restful import Resource, reqparse, marshal
+from flask_restful import Api
 from flask_cors import CORS
 from controller.chat_controller import endpoints
 from controller.admin_controller import adminendpoints
 from controller.pii_controller import piiendpoints
 from controller.userdata_controller import userdataendpoints
 from controller.documents_controller import documentsendpoints
-from flask_smorest import Api
+# from flask_smorest import Api
 import secrets
+from flask_swagger_ui import get_swaggerui_blueprint
 from oidc import oidc
 from globals import *
 
@@ -24,34 +25,31 @@ def configure_logging():
 
 
 app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "AI-Guardrails Swagger"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api/documentation" #url for documentation
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-app.config['API_SPEC_OPTIONS'] = {
-        'security':[{"bearerAuth": []}],
-        'components':{
-            "securitySchemes":
-                {
-                    "bearerAuth": {
-                        "type":"http",
-                        "scheme": "bearer",
-                        "bearerFormat": "JWT"
-                    }
-                }
-        }
-    }
 
 logging.info('client_secret', Globals.oidc_client_id)
+SWAGGER_URL = '/swagger'
+API_URL = 'http://127.0.0.1:8080/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Ai-Harness API documentation"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+app.register_blueprint(endpoints, name="chat", url_prefix="/api/chat")
+app.register_blueprint(piiendpoints, name="pii", url_prefix="/api/pii")
+app.register_blueprint(userdataendpoints, name="user", url_prefix="/api/user")
+app.register_blueprint(adminendpoints, name="admin", url_prefix="/api/admin")
+app.register_blueprint(documentsendpoints, name="docs", url_prefix="/api/docs")
 
-api=Api(app)
-api.register_blueprint(endpoints, name="chat", url_prefix="/api/chat")
-api.register_blueprint(piiendpoints, name="pii", url_prefix="/api/pii")
-api.register_blueprint(userdataendpoints, name="user", url_prefix="/api/user")
-api.register_blueprint(adminendpoints, name="admin", url_prefix="/api/admin")
-api.register_blueprint(documentsendpoints, name="docs", url_prefix="/api/docs")
+@app.route('/swagger.json')
+def swagger():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    swagger_json_path = os.path.join(current_directory, 'swagger.json')
+    with open(swagger_json_path, 'r') as f:
+        return jsonify(json.load(f))
 
 CORS(app)
 cors = CORS(app, resource={
