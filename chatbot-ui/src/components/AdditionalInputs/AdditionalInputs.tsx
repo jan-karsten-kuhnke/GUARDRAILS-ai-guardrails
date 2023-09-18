5f866414587fdd6851864a59730191c2a041cddb
Tim-Kane-65641 <mohdtaha.khan@synergy-ai.us>
2023-09-18 13:21:20 +0530

    mandatory http/https regex modify

--- chatbot-ui/src/components/AdditionalInputs/AdditionalInputs.tsx
+++ chatbot-ui/src/components/AdditionalInputs/AdditionalInputs.tsx
@@ -42,7 +42,7 @@ const AdditionalInputs: FC<Props> = ({ inputs, handleSend }) => {
     setInputUrl(url);
     // Regular expression to validate a URL
     const urlRegex = new RegExp(
-      "^(https?:\\/\\/)?" + // protocol
+      "^(https?:\\/\\/)" + // protocol
         "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
         "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
         "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
